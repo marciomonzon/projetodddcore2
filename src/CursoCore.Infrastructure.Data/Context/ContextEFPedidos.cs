fda2bfdd193759fe970cbf2ba591c18ad53c380e
Marcio <marcio.monzon@smsolucao.com.br>
2018-12-23 15:55:11 -0200

    Contexto

---
+++ src/CursoCore.Infrastructure.Data/Context/ContextEFPedidos.cs
@@ -0,0 +1,29 @@
+﻿using CursoCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CursoCore.Infrastructure.Data.Context
+{
+    public class ContextEFPedidos : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItensPedido> itensPedidos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseMySql(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
